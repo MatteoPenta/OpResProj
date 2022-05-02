047f98da13fde1505f31dbb5b1b0398812c343fc
*[MatteoPenta]* <*[matteopx@gmail.com]*>
2022-05-02 13:45:24 +0200

    Created agent file

---
+++ agents/heuGroup2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import time
+from agents import *
+
+
+class HeuGroup2(Agent):
+
+    def __init__(self, env):
+        self.name = "HeuGroup2"
+        self.env = env
+
+    def compute_delivery_to_crowdship(self, deliveries):
+        return [i + 1 for i in range(len(deliveries))]
+
+    def compute_VRP(self, delivery_to_do, vehicles):
+        ris = []
+        return ris
+
+    def learn_and_save(self):
+        time.sleep(7)
+    
+    def start_test(self):
+        pass
